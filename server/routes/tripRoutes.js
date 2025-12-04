05f8e6011d8d00e68076b321a3e363e27d90e785
vishwabandhu kn <vishwabandhukn@gmail.com>
2025-12-05 03:46:17 +0530

    second commit

--- server/routes/tripRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tripController = require('../controllers/tripController');
-const auth = require('../middleware/auth');
-
-router.post('/', auth, tripController.createTrip);
-router.get('/:userId', auth, tripController.getTrips);
-
-module.exports = router;
